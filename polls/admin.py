6e18fa53e15fc4c7c777577a8ebb05bc3dd2f498
Gaurav Goyal <goyal1092@gmail.com>
2016-08-26 00:17:15 +0530

    Fixed bug in admin formset by renaming fields to fildsets

--- polls/admin.py
+++ polls/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from .models import Question,Choice
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = [
+    fieldsets = [
         ( None,             {'fields': ['question_text']}),
         ('Date information',{'fields': ['pub_date']})
     ]
