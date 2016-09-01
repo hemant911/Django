03aa03c07a610c8ecbc860a478d8f8deb931e684
Hemant Thakur <hemantthakur911@gmail.com>
2016-09-01 07:58:19 +0530

    adding Login

--- polls/urls.py
+++ polls/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import include,url
 
 from . import views
 
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    #URL(r'^login/$',views.login_user,name='login_user'),
 ]
 
 
--- polls/views.py
+++ polls/views.py
@@ -6,6 +6,10 @@ from django.http import HttpResponse,Http404,HttpResponseRedirect
 from django.template import  loader
 from django.urls import reverse
 from django.views import generic
+from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+
 
 
 from .models import Question, Choice
@@ -70,3 +74,16 @@ def detail(request,question_id):
         raise Http404("Question does not exist")
     return render(request, 'polls/detail.html',{'question':question})
 
+def login_user(request):
+    logout(request)
+    username = password = ''
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/main/')
+    return render_to_response('polls/login.html', context_instance=RequestContext(request))
